57139398928ab688811b9c5784ef04d6a4451cd7
goodie <vorkuta@inbox.ru>
2022-10-17 15:42:27 +0300

    task_054 fixed

---
+++ task_054/Program.cs
@@ -0,0 +1,8 @@
+﻿using Lib;
+using static System.Console;
+
+int[,] matrix = Arrays.Create2DArrayInt32(3, 4, 0, 9);
+Arrays.PrintArray(matrix);
+Arrays.BubbleSort(matrix);
+WriteLine();
+Arrays.PrintArray(matrix);
