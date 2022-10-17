8719aa3283a4b49a43025535da3c9ef09889f419
goodie <vorkuta@inbox.ru>
2022-10-17 15:05:14 +0300

    task_060 completed

---
+++ task_060/Program.cs
@@ -0,0 +1,10 @@
+﻿using Lib;
+using static System.Console;
+
+Write("Укажите размер массива x,y,z: ");
+int[] arg = Parser.GetUserInput(",");
+
+int [,,] cube = Arrays.Create3DArrayInt32(arg[0], arg[1], arg[2], 10, 99, true);
+
+if (cube != null) Arrays.Print3DArray(cube);
+else WriteLine("Невозможно заполнить неповторяющимися двузначными числами массив указанного размера");
