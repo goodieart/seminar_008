0db65d3b617829deae342fee4c380b9fade41b0c
goodie <vorkuta@inbox.ru>
2022-10-17 13:52:31 +0300

    task_058 completed

---
+++ task_058/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lib;
+using static System.Console;
+
+WriteLine("m1:");
+int[,] m1 = Arrays.Create2DArrayInt32(2, 2, 1, 4);
+Arrays.PrintArray(m1);
+WriteLine($"\nm2:");
+int[,] m2 = Arrays.Create2DArrayInt32(2, 2, 1, 4);
+Arrays.PrintArray(m2);
+WriteLine($"\nРезультат:");
+int[,] result = Arrays.Array2DMultiplicate(m1, m2);
+
+if (result != null)
+{
+    Arrays.PrintArray(result);
+}
+else
+{
+    WriteLine($"Невозможно вычислить произведение заданных матриц");
+}
