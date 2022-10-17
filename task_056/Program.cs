64436ad3e9ed3b5e8b90bd30ff0055aa14e41a93
goodie <vorkuta@inbox.ru>
2022-10-17 13:29:59 +0300

    task_056 completed

--- task_056/Program.cs
+++ task_056/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lib;
+using static System.Console;
+
+int[,] matrix = Arrays.Create2DArrayInt32(4, 4, 0, 9);
+int[] rowSum = Arrays.SumByRows(matrix);
+Arrays.PrintArray(matrix);
+
+WriteLine($"Строка с наименьшей суммой элементов = {Arrays.GetMinIdx(rowSum) + 1}");
